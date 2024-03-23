922e6c6d3f892018f67ecf0df5b3c527ca6505a7
PantheraNestah <pantheranestah@gmail.com>
2024-03-23 11:17:03 +0300

    separated profile photo upload from other staff details edit form, added notification banners in recent activities

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 import $ from 'jquery'
 import "../scss/style.scss"
 import "bootstrap"
+
+/* $(".input-image").imageUploader({
+    extensions: [".jpg", ".png", ".jpeg"],
+    mimes:[ "image/jpeg", "image/png"],
+    maxsize: 2000,
+    maxfiles: 1,
+    label: "Drag & Drop files here or click to browse"
+}); */
